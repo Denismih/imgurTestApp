5616f81a5364cb8722667d773043e0f61dbe76fa
Den <denismih@gmail.com>
2018-09-30 12:19:56 +0300

    Initial Commit

---
+++ imgurTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imgurTestApp
+//
+//  Created by MacBook Pro on 30.09.2018.
+//  Copyright © 2018 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
